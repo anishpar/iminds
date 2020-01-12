3b076c1874d0094bdd4d070d66bdecdf7f5ba1d1
Suraj Prajapati <suraj.prajapati@sterlite.com>
2020-01-12 14:14:12 +0530

    UI

--- iminds-ui/src/app/modules/feature/notification/components/history/history-view.component.ts
+++ iminds-ui/src/app/modules/feature/notification/components/history/history-view.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MasterComponent } from 'src/app/modules/core/common/components/master.component';
-import { takeUntil } from 'rxjs/operators';
-import {ActivatedRoute} from '@angular/router';
-import {Router} from "@angular/router";
-import { NotificationService } from '../../services/notification.service';
-import { HistoryModel } from '../../models/history.model';
-import { HistoryReceiverModel } from '../../models/history-receiver.model';
-import { HistoryDetailsModel } from '../../models/history-details.model';
-import { DataStoreService } from 'src/app/modules/core/util/services/data-store.service';
+import {Component, OnInit, OnDestroy, ViewChild, ViewContainerRef} from '@angular/core';
+import {MasterComponent} from 'src/app/modules/core/common/components/master.component';
+import {takeUntil} from 'rxjs/operators';
+import {TemplateModel} from '../../models/template.model';
+import {NotificationService} from '../../services/notification.service';
+import {CKEditor4} from 'ckeditor4-angular/ckeditor';
+import {TemplateDetailModel} from '../../models/template-detail.model';
+import {TimeRangeModel} from '../../../shared/models/time-range.model';
+import { EventTags } from '../../models/tags.model';
+import {Department}  from '../../models/department.model';
+import { Location } from '../../models/location.model';
+import {JobStatus}  from '../../models/job.status.model';
+import {EmployeeType}  from '../../models/employeetype.model';
+import {AddJobOpening}  from '../../models/addjobopening.model';
+import {HiringLead}  from '../../models/hiringlead.model';
+import {SkillType}  from '../../models/skilltype.model';
+import {InterviewType}  from '../../models/interviewtype.model';
+import {JobInterviewRel}  from '../../models/jobinterview.model';
+import {JobQuestionsRel}  from '../../models/jobquestions.model';
+import {JobSkillRel}  from '../../models/jobskillrel.model';
+import { EventConfiguration } from '../../models/event-configuration.model';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'stl-history-view.component',
@@ -15,72 +27,195 @@ import { DataStoreService } from 'src/app/modules/core/util/services/data-store.
   })
 
   export class ViewHistoryComponent extends MasterComponent implements OnInit, OnDestroy{
-    public historyData=new HistoryModel();
-    language = '';
-    channel :String = '';
-    recipient :String = '';
-    status :String = '';
-    messageContent = '';
-    statusMessage = '';
-    lastProcessDate = '';
-    languageListFromAPI = [];
-    configurationData = ['languages'];
-    historyList;
+   // @ViewChild('dynamicContainer' , { read: ViewContainerRef }) container: ViewContainerRef;
+  configurationData = ['languages', 'channels', 'events', 'images'];
+  templateModel = new TemplateModel();
+  templateContent = new Array();
+  channels = [];
+  events = [];
+  languages = [];
+  images = [];
+  invalidContent = [];
+  maxContent = [];
+  messageTag = [];
+  rowId: number;
+  status = true;
+  eventTagsAPIResponse = [];
+  eventTag ;
+  listEventTags = [];
+  imageRefId = [];
+  eventConfigurationModel = new EventConfiguration();
+  public defaultParamNameList = [];
+  eventTagModel = [];
+  editor = [];
+  skills: any;
+  jobstatuses = [];
+  jobstatus = new JobStatus();
+
+  hiringLeads = [];
+  hiringLead = new HiringLead();
+
+  departments = [];
+  department = new Department();
+
+  employeeTypes = [];
+  employeeType = new EmployeeType();
+
+  locations = [];
+  location = new Location();
+
+  skillTypes = [];
+  skillType = new SkillType();
+
+  interviewTypes = [];
+  interviewType = new InterviewType();
+
+  jobSkill1 = new JobSkillRel();
+  jobSkill2 = new JobSkillRel();
+  jobSkill3 = new JobSkillRel();
+
+  jobSkills = [];
+
+  addJobOpening = new AddJobOpening();
+
+
+  constructor(public service: NotificationService, private router: Router) {
+    super();
+    this.eventConfigurationModel.templateRelation = [];
+  }
+  ngOnInit() {
+    this.jobstatus.name = 'Draft';
+    this.jobstatuses.push(this.jobstatus);
+    this.jobstatus = new JobStatus();
+    this.jobstatus.name = 'Open';
+    this.jobstatuses.push(this.jobstatus);
+    this.jobstatus = new JobStatus();
+    this.jobstatus.name = 'OnHold';
+    this.jobstatuses.push(this.jobstatus);
+    this.jobstatus = new JobStatus();
+    this.jobstatus.name = 'Filled';
+    this.jobstatuses.push(this.jobstatus);
+    this.jobstatus = new JobStatus();
+    this.jobstatus.name = 'Canceled';
+    this.jobstatuses.push(this.jobstatus);
     
-    constructor(public service: NotificationService,private route:ActivatedRoute,private router: Router ,public dataStoreService :DataStoreService) {
-        super();
-      }
 
-    ngOnInit() {
-      let receiver = [];
-      let tempRecipient;
-      let tempDispatchStatus;
+    this.hiringLead.name = 'Maulik Shah';
+    this.hiringLeads.push(this.hiringLead);
+    this.hiringLead = new HiringLead();
+    this.hiringLead.name = 'Sanjay Madhu';
+    this.hiringLeads.push(this.hiringLead);
+    this.hiringLead = new HiringLead();
+    this.hiringLead.name = 'Pankti Joshipura';
+    this.hiringLeads.push(this.hiringLead);
+    this.hiringLead = new HiringLead();
+    this.hiringLead.name = 'Ajay Iyer';
+    this.hiringLeads.push(this.hiringLead);
 
-      this.historyList = this.getNavParam('transectionId');   
-      if(  this.historyList  != null ){
-        let transectionId =    this.historyList.transectionId;
-        this.historyData = this.dataStoreService.getData(transectionId);
-        if (this.historyData ==null) {
-          // redirect to search page
-          this.router.navigate(['notification/search_history'])
-        }
+    this.department.name = 'SU';
+    this.departments.push(this.department);
+    this.department = new Department();
+    this.department.name = 'DU';
+    this.departments.push(this.department);
+    this.department = new Department();
+    this.department.name = 'HR';
+    this.departments.push(this.department);
+    this.department = new Department();
+    this.department.name = 'Admin';
+    this.departments.push(this.department);
+    this.department = new Department();
+    this.department.name = 'Finance';
+    this.departments.push(this.department);
 
-      for (let i = 0; i < this.historyData.communicationDetails.length; i++) {
-        receiver = [];
-        receiver = this.historyData.communicationDetails[i].receiver;
 
-          for (let j = 0; j < receiver.length; j++) {
-            tempRecipient = receiver[j].communicationAddress;
-            tempDispatchStatus = receiver[j].status;
+    this.employeeType.name = 'Full Time';
+    this.employeeTypes.push(this.employeeType);
+    this.employeeType = new Department();
+    this.employeeType.name = 'Part Time';
+    this.employeeTypes.push(this.employeeType);
+    this.employeeType = new Department();
+    this.employeeType.name = 'Intern';
+    this.employeeTypes.push(this.employeeType);
+    this.employeeType = new Department();
+    this.employeeType.name = 'Contractor';
+    this.employeeTypes.push(this.employeeType);
 
-            if(this.historyList.recipient === tempRecipient && this.historyList.dispatchStatus === tempDispatchStatus){
-              this.channel = this.historyData.communicationDetails[i]["@type"];
-              this.recipient = tempRecipient;
-              this.status = tempDispatchStatus;
-              this.messageContent = this.historyData.communicationDetails[i].content;
-              this.statusMessage = receiver[j].statusMessage;
-              this.lastProcessDate = receiver[j].sendTimeComplete;
-        
-              this.service.loadConfiguration(this.configurationData)
-              .pipe(takeUntil(this.onDestroy$))
-              .subscribe(res => {
-        
-                //filter language cache data to get name of language
-                this.languageListFromAPI = res['languages'];
-                if(this.historyData.language !=null){
-                  this.languageListFromAPI = this.languageListFromAPI.filter(s => s.alias === this.historyData.language);
-                  this.language = this.languageListFromAPI[0].name;
-                }else{
-                  this.language = "-";
-                }  
-              });
-            }
-          }
-         }   
-       } 
-   }
+    this.location.name = 'Ahmedabad';
+    this.locations.push(this.location);
+    this.location = new Location();
+    this.location.name = 'Pune';
+    this.locations.push(this.location);
+    this.location = new Location();
+    this.location.name = 'Delhi';
+    this.locations.push(this.location);
+    this.location = new Location();
+    this.location.name = 'London';
+    this.locations.push(this.location);
+
+    this.skillType.name = 'MinimumSkill';
+    this.skillTypes.push(this.skillType);
+    this.skillType = new SkillType();
+    this.skillType.name = 'PreferredSkill';
+    this.skillTypes.push(this.skillType);
+
+    this.interviewType.name = 'Audio';
+    this.interviewTypes.push(this.interviewType);
+    this.interviewType = new InterviewType();
+    this.interviewType.name = 'Video';
+    this.interviewTypes.push(this.interviewType);
+    this.interviewType = new InterviewType();
+    this.interviewType.name = 'Screening';
+    this.interviewTypes.push(this.interviewType);
+    this.interviewType = new InterviewType();
+    this.interviewType.name = 'Aptitude';
+    this.interviewTypes.push(this.interviewType);
+  }
+
+ 
+  onSubmit() {
     
-    ngOnDestroy() {
-      this.manageDestroy();
-    }
+    this.jobSkills.push(this.jobSkill1);
+    this.jobSkills.push(this.jobSkill2);
+    this.jobSkills.push(this.jobSkill3);
+    this.addJobOpening.jobSkills =  this.jobSkills;
+    console.log(this.addJobOpening);
+    this.service.addJobOpening(this.addJobOpening)
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(res => {
+        this.reset();
+      });
+  } 
+
+  reset() {
+    this.addJobOpening.title = '';
+    this.addJobOpening.jobStatus = '';
+    this.addJobOpening.hiringLead = '';
+    this.addJobOpening.department = '';
+    this.addJobOpening.employeeType = '';
+    this.addJobOpening.jobDescription = '';
+    this.addJobOpening.minimumExp = null;
+    this.addJobOpening.location = '';
+    this.addJobOpening.compensation = '';
+    this.addJobOpening.jobSkills = [];
+    this.addJobOpening.jobQuestionsRels = [];
+    this.addJobOpening.jobInterviewRels = [];
+
+    this.jobSkill1.minimumExp = null;
+    this.jobSkill1.name = '';
+    
+    this.jobSkill2.minimumExp = null;
+    this.jobSkill2.name = '';
+
+    this.jobSkill3.minimumExp = null;
+    this.jobSkill3.name = '';
+  }
+
+  ngOnDestroy() {
+    this.manageDestroy();
+  }
+
+  
+
+  
 }
+
